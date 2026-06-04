fa25641a15770a6ad40f3a676da4b857d4f1ba26
Wadson Vaval <vavalson@hotmail>
2026-06-04 17:55:07 -0400

    Scaffold contact modification addon

---
+++ addons/c3_contact_modification/README.md
@@ -0,0 +1,5 @@
+# C3 Contact Modification
+
+Odoo 19 addon for C3 contact setup customizations.
+
+This scaffold intentionally contains no standalone app, menu, or custom contact workflow. Future issues will extend the standard Contacts app through model and view inheritance.
---
+++ addons/c3_contact_modification/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "C3 Contact Modification",
+    "summary": "Extends Odoo Contacts with C3 contact setup customizations.",
+    "version": "19.0.1.0.0",
+    "category": "Contacts",
+    "author": "C3",
+    "license": "LGPL-3",
+    "depends": ["contacts"],
+    "data": [],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
