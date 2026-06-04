5f58012fb907ac85ba28508c1a6571020b6a04cd
Wadson Vaval <vavalson@hotmail>
2026-06-04 19:51:06 -0400

    Add individual contact name order helper

--- addons/c3_contact_modification/tests/test_res_partner_gender_view.py
+++ addons/c3_contact_modification/tests/test_res_partner_gender_view.py
@@ -7,14 +7,28 @@ from odoo.tests import TransactionCase, tagged
 
 @tagged("post_install", "-at_install")
 class TestResPartnerGenderView(TransactionCase):
-    def test_gender_field_is_present_on_main_contact_form(self):
+    def _get_partner_form_arch(self):
         view = self.env["res.partner"].get_view(
             view_id=self.env.ref("base.view_partner_form").id,
             view_type="form",
         )
-        arch = etree.fromstring(view["arch"].encode())
+        return etree.fromstring(view["arch"].encode())
+
+    def test_gender_field_is_present_on_main_contact_form(self):
+        arch = self._get_partner_form_arch()
 
         gender_fields = arch.xpath("//field[@name='gender']")
 
         self.assertEqual(len(gender_fields), 1)
         self.assertEqual(gender_fields[0].get("invisible"), "is_company")
+
+    def test_individual_name_field_shows_name_order_placeholder(self):
+        arch = self._get_partner_form_arch()
+
+        individual_name_fields = arch.xpath("//field[@name='name' and @invisible='is_company']")
+
+        self.assertEqual(len(individual_name_fields), 1)
+        self.assertEqual(
+            individual_name_fields[0].get("placeholder"),
+            "Last name first, then first name",
+        )
