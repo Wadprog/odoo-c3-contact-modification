22b0e40fe2b212b4e4fba257cf8a5ba46027aaf2
Wadson Vaval <vavalson@hotmail>
2026-06-05 16:09:00 -0400

    feat: add click-to-preview ID viewer

--- addons/c3_contact_modification/static/src/fields/c3_id_document_binary_field.js
+++ addons/c3_contact_modification/static/src/fields/c3_id_document_binary_field.js
@@ -1,5 +1,7 @@
 import { _t } from "@web/core/l10n/translation";
+import { useFileViewer } from "@web/core/file_viewer/file_viewer_hook";
 import { registry } from "@web/core/registry";
+import { url } from "@web/core/utils/urls";
 import { BinaryField, binaryField } from "@web/views/fields/binary/binary_field";
 import { FileUploader } from "@web/views/fields/file_handler";
 
@@ -19,10 +21,33 @@ export class C3IdDocumentFileUploader extends FileUploader {
 }
 
 export class C3IdDocumentBinaryField extends BinaryField {
+    static template = "c3_contact_modification.IdDocumentBinaryField";
     static components = {
         ...BinaryField.components,
         FileUploader: C3IdDocumentFileUploader,
     };
+
+    setup() {
+        super.setup();
+        this.fileViewer = useFileViewer();
+    }
+
+    onFilePreview() {
+        const { name, record } = this.props;
+        const route = `/web/content/${record.resModel}/${record.resId}/${name}`;
+        const queryParams = {
+            filename: this.fileName,
+            filename_field: this.props.fileNameField,
+        };
+        const source = url(route, queryParams);
+        this.fileViewer.open({
+            defaultSource: source,
+            downloadUrl: url(route, { ...queryParams, download: true }),
+            isImage: true,
+            isViewable: true,
+            name: this.fileName,
+        });
+    }
 }
 
 registry.category("fields").add("c3_id_document_binary", {
--- addons/c3_contact_modification/static/tests/fields/c3_id_document_binary_field.test.js
+++ addons/c3_contact_modification/static/tests/fields/c3_id_document_binary_field.test.js
@@ -1,6 +1,7 @@
 import { expect, test } from "@odoo/hoot";
 import { click, setInputFiles, waitFor } from "@odoo/hoot-dom";
 import { animationFrame } from "@odoo/hoot-mock";
+import { getOrigin } from "@web/core/utils/urls";
 import { defineModels, fields, models, mountView } from "@web/../tests/web_test_helpers";
 
 import { C3_ID_DOCUMENT_MAX_SIZE } from "@c3_contact_modification/fields/c3_id_document_binary_field";
@@ -11,7 +12,14 @@ class Partner extends models.Model {
     c3_id_document = fields.Binary();
     c3_id_document_filename = fields.Char();
 
-    _records = [{ id: 1 }];
+    _records = [
+        { id: 1 },
+        {
+            id: 2,
+            c3_id_document: "10",
+            c3_id_document_filename: "identity.png",
+        },
+    ];
 }
 
 defineModels([Partner]);
@@ -27,6 +35,8 @@ const ARCH = `
         <field name="c3_id_document_filename" invisible="1"/>
     </form>
 `;
+
+const READONLY_ARCH = ARCH.replace("<form>", '<form edit="0">');
 
 test("accepts an ID document at the 5 MB limit", async () => {
     await mountView({
@@ -89,4 +99,69 @@ test("preserves standard MIME type validation", async () => {
         "Oops! 'identity.txt' didn’t upload since its format isn’t allowed."
     );
     expect(".o_form_button_save:visible").toHaveCount(0);
+});
+
+test("loads a saved ID document only after Preview is clicked", async () => {
+    await mountView({
+        resModel: "res.partner",
+        resId: 2,
+        type: "form",
+        arch: ARCH,
+    });
+
+    expect(".o_c3_preview_id_document_button").toHaveCount(1);
+    expect(".o-FileViewer").toHaveCount(0);
+    expect(".o-FileViewer-viewImage").toHaveCount(0);
+
+    await click(".o_c3_preview_id_document_button");
+    await animationFrame();
+
+    expect(".o-FileViewer").toHaveCount(1);
+    expect(".o-FileViewer-viewImage").toHaveAttribute(
+        "src",
+        `${getOrigin()}/web/content/res.partner/2/c3_id_document?filename=identity.png&filename_field=c3_id_document_filename`
+    );
+    expect(".o-FileViewer-download a").toHaveAttribute(
+        "href",
+        `${getOrigin()}/web/content/res.partner/2/c3_id_document?filename=identity.png&filename_field=c3_id_document_filename&download=true`
+    );
+});
+
+test("does not display Preview without a saved ID document", async () => {
+    await mountView({
+        resModel: "res.partner",
+        resId: 1,
+        type: "form",
+        arch: ARCH,
+    });
+
+    expect(".o_c3_preview_id_document_button").toHaveCount(0);
+});
+
+test("hides Preview while a replacement ID document is unsaved", async () => {
+    await mountView({
+        resModel: "res.partner",
+        resId: 2,
+        type: "form",
+        arch: ARCH,
+    });
+
+    await click(".o_select_file_button");
+    await animationFrame();
+    const file = new File(["replacement"], "replacement.png", { type: "image/png" });
+    await setInputFiles([file]);
+    await waitFor(".o_form_button_save:visible");
+
+    expect(".o_c3_preview_id_document_button").toHaveCount(0);
+});
+
+test("displays Preview for a saved readonly ID document", async () => {
+    await mountView({
+        resModel: "res.partner",
+        resId: 2,
+        type: "form",
+        arch: READONLY_ARCH,
+    });
+
+    expect(".o_c3_preview_id_document_button").toHaveCount(1);
 });
