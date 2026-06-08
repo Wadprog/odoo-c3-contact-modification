3c39d00faa73a8ee5da91c8181703daecbce748e
Wadson Vaval <vavalson@hotmail>
2026-06-08 11:29:48 -0400

    Add visit translations

--- addons/c3_contact_modification/tests/__init__.py
+++ addons/c3_contact_modification/tests/__init__.py
@@ -14,3 +14,4 @@ from . import test_contact_id_document_audit_log
 from . import test_res_partner_visit_reason
 from . import test_res_partner_visit
 from . import test_res_partner_visit_view
+from . import test_res_partner_visit_translation
